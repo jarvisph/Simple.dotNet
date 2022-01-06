3fb15b21ad4ef3499be26a93e40644174c709237
jarvis <jarvis_ph@mail.com>
2022-01-06 17:12:18 +0800

    添加aes加密解密

---
+++ src/Simple.dotNet.Core/Encryption/AesEncryption.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Simple.dotNet.Core.Encryption
+{
+    /// <summary>
+    /// AES 加密 解密
+    /// </summary>
+    public class AesEncryption
+    {
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="plainText">明文</param>
+        /// <param name="key"></param>
+        /// <param name="iv"></param>
+        /// <param name="cipher">默认CBC</param>
+        /// <param name="pading">默认PKCS7</param>
+        /// <returns></returns>
+        public static string AesEncrypt(string plainText, string key, string iv, CipherMode cipher = CipherMode.CBC, PaddingMode pading = PaddingMode.PKCS7)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            using (RijndaelManaged rmd = new RijndaelManaged())
+            {
+                rmd.Key = Encoding.UTF8.GetBytes(key);
+                rmd.IV = Encoding.UTF8.GetBytes(iv);
+                rmd.Mode = cipher;
+                rmd.Padding = pading;
+                ICryptoTransform encryptor = rmd.CreateEncryptor(rmd.Key, rmd.IV);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (CryptoStream encrypt = new CryptoStream(stream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(encrypt))
+                        {
+                            sw.Write(plainText);
+                        }
+                        encrypted = stream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(encrypted);
+        }
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="plainText">密文</param>
+        /// <param name="key"></param>
+        /// <param name="iv"></param>
+        /// <param name="cipher">默认CBC</param>
+        /// <param name="pading">默认PKCS7</param>
+        /// <returns></returns>
+        public static string AesDecrypt(string plainText, string key, string iv, CipherMode cipher = CipherMode.CBC, PaddingMode pading = PaddingMode.PKCS7)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] textBytes = Convert.FromBase64String(plainText);
+            string plaintext = null;
+            using (RijndaelManaged rmd = new RijndaelManaged())
+            {
+                rmd.Key = Encoding.UTF8.GetBytes(key);
+                rmd.IV = Encoding.UTF8.GetBytes(iv);
+                rmd.Mode = cipher;
+                rmd.Padding = pading;
+                ICryptoTransform decryptor = rmd.CreateDecryptor(rmd.Key, rmd.IV);
+
+                using (MemoryStream stram = new MemoryStream(textBytes))
+                {
+                    using (CryptoStream decrypt = new CryptoStream(stram, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(decrypt))
+                        {
+                            plaintext = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+    }
+}
---
+++ test/Simple.dotNet.Core.Test/Encryption/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Simple.dotNet.Core.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.dotNet.Core.Test.Encryption
+{
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+            var data = new
+            {
+                UserID = 1
+            };
+            string key = "000000007de1dd48017dffb16d650116";
+            string iv = key.Substring(7, 16);
+            string value = @"hGqt1Coq3BdMyGFK6OiDtvDWkSJHLXvbYBpEzjZfRX1idFAAuKcHeLTuhTfTVj+qpdp1poC5yTCC
+QGKU + Z2rbyJKRvruu4okbmAkVppti2OgjSUPYChLP + tUCTsppyBG + RZJqY2RQ0fItvHKpz99OBUe
+tfWjYQ20ri0ZgVIW + eU = ";
+            //var decrypt = DesEncryption.AesEncrypt(JsonConvert.SerializeObject(data), key, iv);
+            string v = DesEncryption.AesDecrypt(value, key, iv);
+            Console.WriteLine(v);
+        }
+
+    }
+}
