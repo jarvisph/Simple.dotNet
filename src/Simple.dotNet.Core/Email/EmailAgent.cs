df18cbb5f8e865cec2bbaf21e4eabbce01292518
jarvis <jarvis_ph@mail.com>
2026-07-17 12:29:48 +0800

    11

--- src/Simple.dotNet.Core/Email/EmailAgent.cs
+++ src/Simple.dotNet.Core/Email/EmailAgent.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Simple.Core.Email
 {
